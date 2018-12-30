9ad21caf055822f3670f27414c89729e1b169e91
jwjung <jjwonyop@gmail.com>
2018-12-30 15:44:53 +0900

    Add Create Event Test

--- src/main/java/com/example/demorests/events/Event.java
+++ src/main/java/com/example/demorests/events/Event.java
@@ -23,10 +23,11 @@ public class Event {
     private LocalDateTime closeEnrollmentDateTime;
     private LocalDateTime beginEventDateTime;
     private LocalDateTime endEventDateTime;
-    private String location; // (optional) 이게 없으면 온라인 모임 private int basePrice; // (optional)
+    private String location; // (optional) 이게 없으면 온라인 모임
+    private int basePrice; // (optional)
     private int maxPrice; // (optional)
     private int limitOfEnrollment;
     private boolean offline;
     private boolean free;
-    private EventStatus eventStatus;
+    private EventStatus eventStatus = EventStatus.DRAFT;
 }
